25ecabfeb0d88c5c1906e714a1d6a499a16e5688
priyalakhani291 <166088862+priyalakhani291@users.noreply.github.com>
2024-04-06 13:36:41 +0530

    pr-7

---
+++ js/project.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+
+  var counters = $(".count");
+  var countersQuantity = counters.length;
+  var counter = [];
+
+  for (i = 0; i < countersQuantity; i++) {
+    counter[i] = parseInt(counters[i].innerHTML);
+  }
+
+  var count = function (start, value, id) {
+    var localStart = start;
+    setInterval(function () {
+      if (localStart < value) {
+        localStart++;
+        counters[id].innerHTML = localStart;
+      }
+    }, 40);
+  }
+
+  for (j = 0; j < countersQuantity; j++) {
+    count(0, counter[j], j);
+  }
+});
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: "1",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 4,
+    slideShadows: false
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 4
+    }
+  }
+});
